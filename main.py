de36d781e1c35244026694c26592f790389c2014
Patrick Pierson <patrickpierson@users.noreply.github.com>
2023-10-08 18:29:26 +0000

    fixes issues

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+"""Module providing an example python app."""
 from flask import Flask
 from flask import jsonify
 
@@ -5,9 +6,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    """Function returning hello world."""
     return 'Hello, World!'
 
 @app.route('/json')
 def json_hw():
+    """Function returning json data."""
     data = {"name": "John Doe", "age": 30}
     return jsonify(data)
